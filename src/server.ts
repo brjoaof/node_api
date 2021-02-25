1c47ca2cd052232b189602d16e225fb175afb0ad
brjoaof <brjoaof@gmail.com>
2021-02-24 23:54:44 -0300

    Aula 2 - Banco de Dados

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
+import "reflect-metadata";
 import express from "express";
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
+app.use(express.json());
+app.use(router);
 /**
  * GET -> Buscar
  * POST -> Salvar
@@ -12,17 +17,17 @@ const app = express();
 
  // http://localhost:3333/users
 
- app.get("/", (request, response) => {
-    //  return response.send("Hello World - NLW04");
-    return response.json({ message: "Hello World - NLW04"})
- });
+//  app.get("/", (request, response) => {
+//     //  return response.send("Hello World - NLW04");
+//     return response.json({ message: "Hello World - NLW04"})
+//  });
 
- // 1º param ->   Rota(Recurso API)
- // 2º param -> request, response
- app.post("/", (request, response) => {
-    // Recebeu os dados para salvar
-    return response.json({message: "Os dados foram salvos com sucesso!"});
- });
+//  // 1º param ->   Rota(Recurso API)
+//  // 2º param -> request, response
+//  app.post("/", (request, response) => {
+//     // Recebeu os dados para salvar
+//     return response.json({message: "Os dados foram salvos com sucesso!"});
+//  });
 
 app.listen(3333, () => console.log("Server is running!"));
 
